efb4503b2bbb479a5b9cc7ec3a7503ad3887c855
Richienv <kidnovell.richie@gmail.com>
2024-12-20 15:40:27 +0800

    chore: update changes

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,5 +1,7 @@
-import { Analytics } from '@vercel/analytics/react';
+'use client';
+
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
 export function AnalyticsWrapper() {
-  return <Analytics />;
+  return <VercelAnalytics />;
 } 
